def9aa73567a4e74343fbdebeefb877726ea2909
WebMobik <ddimon3000@bk.ru>
2020-10-21 19:47:56 +0300

    try to add resize window functional

--- src/components/display/comparisons.js
+++ src/components/display/comparisons.js
@@ -1,66 +1,65 @@
 export function initComparisons() {
   const x = document.querySelector('.img-comp-overlay');
+
   compareImages(x);
-  function compareImages(img) {
-    let clicked = 0
-    const w = img.offsetWidth;
-    /* set the width of the img element to 50%:*/
-    img.style.width = (w / 2) + 'px';
-    console.log(img.style.width)
-    /* create slider:*/
-    const slider = document.createElement('div');
-    slider.setAttribute('class', 'img-comp-slider');
-    /* insert slider*/
-    img.parentElement.insertBefore(slider, img);
-    slider.style.left = (w / 2) - (slider.offsetWidth / 2) + 'px';
-    /* execute a function when the mouse button is pressed:*/
-    slider.addEventListener('mousedown', slideReady);
-    /* and another function when the mouse button is released:*/
-    window.addEventListener('mouseup', slideFinish);
-    /* or touched (for touch screens:*/
-    function slideReady(e) {
-      /* prevent any other actions
-        that may occur when moving over the image:*/
-      e.preventDefault();
-      /* the slider is now clicked and ready to move:*/
-      clicked = 1;
-      /* execute a function when the slider is moved:*/
-      window.addEventListener('mousemove', slideMove);
-    }
-    function slideFinish() {
-      /* the slider is no longer clicked:*/
-      clicked = 0;
-    }
-    function slideMove(e) {
-      let pos;
-      /* if the slider is no longer clicked, exit this function:*/
-      if (clicked == 0) return false;
-      /* get the cursor's x position:*/
-      pos = getCursorPos(e)
-      /* prevent the slider from being positioned outside the image:*/
-      if (pos < 0) pos = 0;
-      if (pos > w) pos = w;
-      /* execute a function that will
-        resize the overlay image according to the cursor:*/
-      slide(pos);
-    }
-    function getCursorPos(e) {
-      let a = ''
-      let x = 0;
-      e = e || window.event;
-      /* get the x positions of the image:*/
-      a = img.getBoundingClientRect();
-      /* calculate the cursor's x coordinate, relative to the image:*/
-      x = e.pageX - a.left;
-      /* consider any page scrolling:*/
-      x = x - window.pageXOffset;
-      return x;
-    }
-    function slide(x) {
-      /* resize the image:*/
-      img.style.width = x + 'px';
-      /* position the slider:*/
-      slider.style.left = img.offsetWidth - (slider.offsetWidth / 2) + 'px';
-    }
+}
+
+function compareImages(img) {
+  let clicked = 0
+  const w = img.offsetWidth;
+
+  img.style.width = (w / 2) + 'px';
+
+  const slider = document.querySelector('.img-comp-slider')
+
+  img.parentElement.insertBefore(slider, img);
+  slider.style.left = (w / 2) - (slider.offsetWidth / 2) + 'px';
+
+  slider.addEventListener('mousedown', slideReady);
+
+  window.addEventListener('mouseup', slideFinish);
+
+  function slideReady(e) {
+    e.preventDefault();
+
+    clicked = 1;
+
+    window.addEventListener('mousemove', slideMove);
+  }
+
+  function slideFinish() {
+    clicked = 0;
+  }
+
+  function slideMove(e) {
+    let pos;
+
+    if (clicked == 0) return false;
+
+    pos = getCursorPos(e)
+
+    if (pos < 0) pos = 0;
+    if (pos > w) pos = w;
+
+    slide(pos);
+  }
+
+  function getCursorPos(e) {
+    let a = ''
+    let x = 0;
+    e = e || window.event;
+
+    a = img.getBoundingClientRect();
+
+    x = e.pageX - a.left;
+
+    x = x - window.pageXOffset;
+    return x;
+  }
+
+  function slide(x) {
+    img.style.width = x + 'px';
+
+    slider.style.left = img.offsetWidth - (slider.offsetWidth / 2) + 'px';
   }
 }
--- src/components/display/display.js
+++ src/components/display/display.js
@@ -17,16 +17,15 @@ export class Display extends EditorComponent {
 
     toHTML() {
       return `
-            <div class="resize-img">
-                <div class="img-comp-img">
-                    <img src="https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__340.jpg" alt="editing">
-                </div>
-                <div class="img-comp-img img-comp-overlay">
-                    <img src="https://cdn.pixabay.com/photo/2015/02/24/15/41/dog-647528__340.jpg" alt="editing">
-                </div>
-            </div>
+          <div class="resize-img">
+              <div class="img-comp-img">
+                <img src="https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__340.jpg" alt="editing">
+              </div>
+              <div class="img-comp-img img-comp-overlay">
+                <img src="https://cdn.pixabay.com/photo/2015/02/24/15/41/dog-647528__340.jpg" alt="editing">
+              </div>
+              <div class="img-comp-slider"></div>
+          </div>
         `
     }
 }
-
-
